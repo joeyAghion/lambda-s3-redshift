d998c0b775dbf695882910cf3637e4faf137aa7c
Joey Aghion <joey@aghion.com>
2015-11-18 16:31:42 -0500

    Retry transaction 3 times before failing Lambda function

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ This [AWS Lambda](https://aws.amazon.com/lambda/) function loads [gzipped] JSON 
     nvm install 0.10.36 # version supported by AWS Lambda
     nvm use 0.10.36
     npm install
-    cp config.json.example config.json # and edit to suit needs
-    npm run simulate # optional, to simulate Lambda function (WARNING: accesses _real_ S3/Redshift resources)
+    cp config.example.json config.json # and edit to suit needs
     npm run setup # optional, to create staging table
+    npm run simulate # optional, to simulate Lambda function (WARNING: accesses _real_ S3/Redshift resources)
     npm run zip # package for upload to AWS
 
 ## Configuration
--- index.js
+++ index.js
@@ -1,46 +1,49 @@
-// Main Lambda function. Inserts row for new file into stagingTable, then loads into destinationTable.
+// Main Lambda function. Inserts row for new file into stagingTable, then loads file contents into destinationTable.
 
 var aws = require('aws-sdk');
 var pg = require('pg');
+var async = require('async');
 var config = require('./config.json');
 var s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 exports.handler = function(event, context) {
   var db = new pg.Client(config.connectionString);
-  var fail = function(err, client) {
-    console.error("Failure: " + err);
-    client.query('ROLLBACK', function() {
-      client.end();
-      context.fail(err);
+  var s3 = event.Records[0].s3;
+  var tries = 0;
+  var rollback = function(err, callback) {
+    console.error("Rolling back...", err);
+    db.query('ROLLBACK', function() {
+      callback(err);
     });
   };
-  var succeed = function(message, client) {
-    console.log("Success: " + message);
-    client.query('COMMIT', function(err, result) {
-      if (err) return fail(err, client);
-      client.end();
-      context.succeed(message);
+  var commit = function(message, callback) {
+    db.query('COMMIT', function(err, result) {
+      if (err) return rollback(err, callback);
+      console.log("Committing...", message);
+      callback(null, message);
     });
   };
-  var loadFromS3 = function(s3) {
+  var loadFromS3 = function(callback) {
+    console.log("Started loading " + s3.object.key + " (try " + ++tries + ")");
     db.query('BEGIN', function(err, result) {
-      if (err) return fail(err, db);
+      if (err) return callback(err);
+      var select = "SELECT";
       db.query("SELECT 1 FROM " + config.stagingTable + " WHERE key = $1", [s3.object.key], function(err, result) {
-        if (err) return fail(err, db);
+        if (err) return rollback(err, callback);
         if (result.rows.length > 0) {
-          succeed("Item with key " + s3.object.key + " already exists. Exiting.", db);
+          return commit("Item with key " + s3.object.key + " already exists.", callback);
         } else {
           db.query('INSERT INTO ' + config.stagingTable + '(key) VALUES($1)', [s3.object.key], function(err, result) {
-            if (err) return fail(err, db);
+            if (err) return rollback(err, callback);
             var command = "COPY " + config.destinationTable +
               " FROM 's3://" + s3.bucket.name + "/" + s3.object.key + "'" +
               "CREDENTIALS 'aws_access_key_id=" + config.awsAccessKeyId + ";aws_secret_access_key=" + config.awsSecretAccessKey + "'" +
               "FORMAT AS json 'auto' GZIP timeformat 'auto'";
             db.query(command, function(err, result) {
-              if (err) return fail(err, db);
+              if (err) return rollback(err, callback);
               db.query("UPDATE " + config.stagingTable + " SET completed_at = GETDATE() WHERE key = $1", [s3.object.key], function(err, result) {
-                if (err) return fail(err, db);
-                succeed("Loaded " + s3.object.key, db);
+                if (err) return rollback(err, callback);
+                commit("Loaded " + s3.object.key, callback);
               });
             });
           });
@@ -49,9 +52,12 @@ exports.handler = function(event, context) {
     });
   };
 
-  console.log("Started loading " + event.Records[0].s3.object.key);
   db.connect(function(err) {
-    if (err) context.fail(err);
-    loadFromS3(event.Records[0].s3);
+    if (err) return context.done(err);
+    async.retry({times: config.retries, interval: config.retryInterval}, loadFromS3, function(err, result) {
+      console.log("Completed", {tries: tries, err: err, result: result});
+      db.end();
+      context.done(err, result);
+    });
   });
 };
